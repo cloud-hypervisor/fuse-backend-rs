cloud-hypervisor/fuse-backend-rs

commit 07cda678384db6d0f0829ced764ea13697307d98

passthroughfs: don't change credentials during write
We changed credentials during a write because we want to clear suid/sgid
bits "automatically" to satisfy the semantics of HANDLE_KILLPRIV
capability, as the fuse module won't send explicit request to do so.

But this doesn't work well with no_open option, e.g.

  int fd = open("testfile", O_TRUNC | O_CREAT | O_RDWR, 0444);
  write(fd, "hello", 5);

write(2) will get EACCES when no_open is enabled, because we first
create a file with 0444 mode, and open it again with O_RDWR and result
in EACCES. A strace log looks like:

 # The first open with O_CREAT
 setresgid(4294967295, 1000, 4294967295) = 0
 setresuid(4294967295, 1000, 4294967295) = 0
 openat(78, "testfile", O_RDWR|O_CREAT|O_EXCL|O_LARGEFILE|O_NOFOLLOW|O_CLOEXEC, 0100444) = 82
 openat(78, "testfile", O_RDONLY|O_NOFOLLOW|O_CLOEXEC|0x200000) = 83
 newfstatat(83, "", {st_mode=S_IFREG|0444, st_size=0, ...}, AT_SYMLINK_NOFOLLOW|AT_EMPTY_PATH) = 0
 close(82)                   = 0
 setresgid(4294967295, 0, 4294967295) = 0
 setresuid(4294967295, 0, 4294967295) = 0

 # The write(2) triggers a second open when no_open is true
 setresgid(4294967295, 1000, 4294967295) = 0
 setresuid(4294967295, 1000, 4294967295) = 0
 openat(79, "self/fd/83", O_RDWR|O_CLOEXEC) = -1 EACCES (Permission denied)
 setresgid(4294967295, 0, 4294967295) = 0
 setresuid(4294967295, 0, 4294967295) = 0

As we've already disabled HANDLE_KILLPRIV in previous patch, there's no
need to change credentials during write either, and do the open as
root, who has CAP_DAC_OVERRIDE to bypass the permission check.

This also matches what virtiofsd does.

Fixes: #31533336
Signed-off-by: Eryu Guan <eguan@linux.alibaba.com>

src/passthrough/fs.rs
@@ -1111,10 +1111,6 @@ impl FileSystem for PassthroughFs {
         _delayed_write: bool,
         _flags: u32,
     ) -> io::Result<usize> {
-        // We need to change credentials during a write so that the kernel will remove setuid or
-        // setgid bits from the file if it was written to by someone other than the owner.
-        let (_uid, _gid) = set_creds(ctx.uid, ctx.gid)?;
-
         let data = self.get_data(handle, inode, libc::O_RDWR)?;
 
         // This is safe because read_to uses pwritev64, so the underlying file descriptor

commit 9f1016f7ffbc7cb900cefb0df3af64614b10e675

passthroughfs: fix build warnings
ctx is not used anymore.

Fixes: 07cda678384d ("passthroughfs: don't change credentials during write")
Signed-off-by: Eryu Guan <eguan@linux.alibaba.com>

src/passthrough/fs.rs
@@ -1101,7 +1101,7 @@ impl FileSystem for PassthroughFs {
 
     fn write(
         &self,
-        ctx: Context,
+        _ctx: Context,
         inode: Inode,
         handle: Handle,
         r: &mut dyn ZeroCopyReader,