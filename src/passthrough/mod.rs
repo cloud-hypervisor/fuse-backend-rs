4db5e242a77d6e5f6f79fa1157601a443f9b6af2
Liu Jiang <gerry@linux.alibaba.com>
2020-12-18 17:38:58 +0800

    passthroughfs: simple syntax/doc improvements
    Simple syntax/doc improvements, there should be no functionality
    changes.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- src/passthrough/mod.rs
+++ src/passthrough/mod.rs
@@ -1,7 +1,15 @@
 // Copyright (C) 2020 Alibaba Cloud. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//! Fuse lowlevel passthrough implementation.
+//! Fuse passthrough file system, mirroring an existing FS hierarchy.
+//!
+//! This file system mirrors the existing file system hierarchy of the system, starting at the
+//! root file system. This is implemented by just "passing through" all requests to the
+//! corresponding underlying file system.
+//!
+//! The code is derived from the
+//! [CrosVM](https://chromium.googlesource.com/chromiumos/platform/crosvm/) project,
+//! with heavy modification/enhancements from Alibaba Cloud OS team.
 
 mod fs;
 mod multikey;
