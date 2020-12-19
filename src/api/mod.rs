a01e84dcd231776fb399b7f930d3fd9c9f607e0a
Liu Jiang <gerry@linux.alibaba.com>
2020-12-19 23:07:49 +0800

    api: improve documentation
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,9 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Fuse Application Programming Interfaces(API).
+//!
+//! The Fuse application programming interfaces(API) layer is an intermediate layer
+//! between the transport layer and the backend file system drivers. It provides:
+//! - [struct Server](server/struct.Server.html) to receive requests from/send reply to the
+//!   transport layer.
+//! - [trait FileSystem](filesystem/trait.FileSystem.html) for backend file system drivers to
+//!   implement fs operations.
+//! - [struct Vfs](vfs/struct.Vfs.html), a simple union file system to help organize multiple
+//!   backend file systems.
 
 mod pseudo_fs;
-mod vfs;
+pub mod vfs;
 
 pub use vfs::{BackendFileSystem, Vfs, VfsOptions, VFS_MAX_INO};
 pub mod filesystem;
