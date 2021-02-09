cd960a358fe09fa5a758549288b105c525811173
Peng Tao <bergwolf@hyper.sh>
2021-02-09 14:29:37 +0800

    tests: add basic integration tests
    Add a passthroughfs example daemon and use it to run basic integration
    tests.
    
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

---
+++ tests/example/passthroughfs.rs
@@ -0,0 +1,136 @@
+// Copyright 2021 Ant Group. All rights reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use std::io::Result;
+use std::path::Path;
+use std::sync::Arc;
+use std::thread;
+
+use vmm_sys_util::eventfd::EventFd;
+
+use fuse_rs::api::{server::Server, Vfs, VfsOptions};
+use fuse_rs::passthrough::{Config, PassthroughFs};
+use fuse_rs::transport::fusedev;
+
+/// A fusedev daemon example
+pub struct Daemon {
+    mountpoint: String,
+    server: Arc<Server<Arc<Vfs>>>,
+    thread_cnt: u32,
+    event_fd: EventFd,
+    session: Option<fusedev::FuseSession>,
+}
+
+impl Daemon {
+    /// Creates a fusedev daemon instance
+    pub fn new(src: &str, mountpoint: &str, thread_cnt: u32) -> Result<Self> {
+        // create vfs
+        let vfs = Vfs::new(VfsOptions {
+            no_open: false,
+            no_opendir: false,
+            ..Default::default()
+        });
+
+        // create passthrough fs
+        let mut cfg = Config::default();
+        cfg.root_dir = src.to_string();
+        cfg.do_import = false;
+        let fs = PassthroughFs::new(cfg).unwrap();
+        fs.import().unwrap();
+
+        // attach passthrough fs to vfs root
+        vfs.mount(Box::new(fs), "/").unwrap();
+
+        Ok(Daemon {
+            mountpoint: mountpoint.to_string(),
+            server: Arc::new(Server::new(Arc::new(vfs))),
+            thread_cnt,
+            event_fd: EventFd::new(0).unwrap(),
+            session: None,
+        })
+    }
+
+    /// Mounts a fusedev daemon to the mountpoint, then start service threads to handle
+    /// FUSE requests.
+    pub fn mount(&mut self) -> Result<()> {
+        let mut se =
+            fusedev::FuseSession::new(Path::new(&self.mountpoint), "passthru_example", "").unwrap();
+        se.mount().unwrap();
+        for _ in 0..self.thread_cnt {
+            let server = FuseServer {
+                server: self.server.clone(),
+                ch: se.new_channel(self.event_fd.try_clone().unwrap()).unwrap(),
+            };
+            let _thread = thread::Builder::new()
+                .name("fuse_server".to_string())
+                .spawn(move || {
+                    info!("new fuse thread");
+                    let _ = server.svc_loop();
+                    warn!("fuse service thread exits");
+                })
+                .unwrap();
+        }
+        self.session = Some(se);
+        Ok(())
+    }
+
+    /// Umounts and destroies a fusedev daemon
+    pub fn umount(&mut self) -> Result<()> {
+        if let Some(mut se) = self.session.take() {
+            se.umount().unwrap();
+        }
+        self.event_fd.write(1)?;
+        Ok(())
+    }
+}
+
+impl Drop for Daemon {
+    fn drop(&mut self) {
+        let _ = self.umount();
+    }
+}
+
+struct FuseServer {
+    server: Arc<Server<Arc<Vfs>>>,
+    ch: fusedev::FuseChannel,
+}
+
+impl FuseServer {
+    fn svc_loop(&self) -> Result<()> {
+        let mut buf = Vec::with_capacity(1024 * 1024);
+        unsafe {
+            buf.set_len(1024 * 1024);
+        }
+
+        // Given error EBADF, it means kernel has shut down this session.
+        let _ebadf = std::io::Error::from_raw_os_error(libc::EBADF);
+        loop {
+            if let Some(reader) = self
+                .ch
+                .get_reader(&mut buf)
+                .map_err(|_| std::io::Error::from_raw_os_error(libc::EINVAL))?
+            {
+                let writer = self
+                    .ch
+                    .get_writer()
+                    .map_err(|_| std::io::Error::from_raw_os_error(libc::EINVAL))?;
+                if let Err(e) = self.server.handle_message(reader, writer, None, None) {
+                    match e {
+                        fuse_rs::Error::EncodeMessage(_ebadf) => {
+                            break;
+                        }
+                        _ => {
+                            error!("Handling fuse message failed");
+                            continue;
+                        }
+                    }
+                }
+            } else {
+                info!("fuse server exits");
+                break;
+            }
+        }
+        Ok(())
+    }
+}
---
+++ tests/smoke.rs
@@ -0,0 +1,101 @@
+// Copyright 2020-2021 Ant Group. All rights reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#[cfg(feature = "fusedev")]
+#[macro_use]
+extern crate log;
+
+mod example;
+
+#[cfg(feature = "fusedev")]
+mod fusedev_tests {
+    extern crate stderrlog;
+
+    use std::io::Result;
+    use std::path::Path;
+    use std::process::Command;
+
+    use vmm_sys_util::tempdir::TempDir;
+
+    use crate::example::passthroughfs;
+
+    fn validate_two_git_directory(src: &str, dest: &str) -> bool {
+        let src_files =
+            exec(format!("cd {}; git ls-files;cd - > /dev/null", src).as_str()).unwrap();
+        let dest_files =
+            exec(format!("cd {}; git ls-files;cd - > /dev/null", dest).as_str()).unwrap();
+        if src_files != dest_files {
+            error!(
+                "src {}:\n{}\ndest {}:\n{}",
+                src, src_files, dest, dest_files
+            );
+            return false;
+        }
+
+        let src_md5 =
+            exec(format!("cd {}; git ls-files | xargs md5sum; cd - > /dev/null", src).as_str())
+                .unwrap();
+        let dest_md5 =
+            exec(format!("cd {}; git ls-files | xargs md5sum; cd - > /dev/null", dest).as_str())
+                .unwrap();
+        if src_md5 != dest_md5 {
+            error!("src {}:\n{}\ndest {}:\n{}", src, src_md5, dest, dest_md5,);
+            return false;
+        }
+
+        return true;
+    }
+
+    fn exec(cmd: &str) -> Result<String> {
+        debug!("exec: {}", cmd);
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg(cmd)
+            .env("RUST_BACKTRACE", "1")
+            .output()?;
+
+        if !output.status.success() || output.stderr.len() > 0 {
+            panic!(format!(
+                "exec failed: {}: {}",
+                cmd,
+                std::str::from_utf8(&output.stderr).unwrap()
+            ));
+        }
+        let stdout = std::str::from_utf8(&output.stdout).unwrap();
+
+        return Ok(stdout.to_string());
+    }
+
+    #[test]
+    fn integration_test_init() -> Result<()> {
+        stderrlog::new()
+            .quiet(false)
+            .timestamp(stderrlog::Timestamp::Microsecond)
+            .verbosity(log::LevelFilter::Trace as usize - 1)
+            .init()
+            .unwrap();
+        Ok(())
+    }
+
+    #[test]
+    fn integration_test_tree_gitrepo() -> Result<()> {
+        // test the fuse-rs repository
+        let src = Path::new(".").canonicalize().unwrap();
+        let src_dir = src.to_str().unwrap();
+        let tmp_dir = TempDir::new().unwrap();
+        let mnt_dir = tmp_dir.as_path().to_str().unwrap();
+        info!(
+            "test passthroughfs src {:?} mountpoint {}",
+            src_dir, mnt_dir
+        );
+
+        let mut daemon = passthroughfs::Daemon::new(src_dir, mnt_dir, 2).unwrap();
+        daemon.mount().unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        assert!(validate_two_git_directory(src_dir, mnt_dir));
+        daemon.umount().unwrap();
+        Ok(())
+    }
+}
