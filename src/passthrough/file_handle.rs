dc1c6eebf539baae9e2bd0e84a8a82c4882b57a6
Liu Bo <bo.liu@linux.alibaba.com>
2021-04-15 09:29:25 +0200

    passthroughfs: File handles as an alternative to O_PATH files
    This is the first patch to allow using file handles in `InodeData` as an
    alternative to the `O_PATH` `File` we used to store always store there.
    
    The `FileOrHandle` enum is extended by a `Handle` variant.
    `InodeData.get_file()` and `InodeData.open_file()` are extended so they
    can handle such a handle.
    
    Furthermore, this patch adds `PassthroughFs.mount_fds`, which is a hash
    map that maps mount IDs to file descriptors: A file handle is valid only
    in the context of a specific mount, so `name_to_handle_at()` returns an
    identifying mount ID, and we store this ID in `FileHandle`.  However,
    when opening a handle, an FD has to be supplied that refers to an open
    inode on the mount in question.  `mount_fds` performs this mapping.
    
    Note that this patch does not yet create any file handles, and so
    `mount_fds` remains empty and the `FileOrHandle::Handle` variant is dead
    code.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>

--- src/passthrough/file_handle.rs
+++ src/passthrough/file_handle.rs
@@ -2,16 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE-BSD-3-Clause file.
 
+use std::collections::HashMap;
 use std::ffi::CStr;
 use std::fs::File;
 use std::io;
 use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::sync::RwLock;
 
 /// An arbitrary maximum size for CFileHandle::f_handle.
 ///
 /// According to Linux ABI, struct file_handle has a flexible array member 'f_handle', but it's
 /// hard-coded here for simplicity.
 const MAX_HANDLE_SZ: usize = 128;
+
+/**
+ * Creating a file handle only returns a mount ID; opening a file handle requires an open fd on the
+ * respective mount.  This is a type in which we can store fds that we know are associated with a
+ * given mount ID, so that when opening a handle we can look it up.
+ */
+#[derive(Default)]
+pub struct MountFds {
+    map: RwLock<HashMap<u64, File>>,
+}
 
 #[repr(C)]
 struct CFileHandle {
@@ -34,7 +46,6 @@ impl CFileHandle {
 }
 
 pub struct FileHandle {
-    #[allow(dead_code)]
     mnt_id: u64,
     handle: CFileHandle,
 }
@@ -55,6 +66,12 @@ extern "C" {
         file_handle: *const CFileHandle,
         flags: libc::c_int,
     ) -> libc::c_int;
+}
+
+impl MountFds {
+    pub fn new() -> Self {
+        MountFds::default()
+    }
 }
 
 impl FileHandle {
@@ -89,7 +106,6 @@ impl FileHandle {
      * `mount_fd` must be an open non-`O_PATH` file descriptor for an inode on the same mount as
      * the file to be opened, i.e. the mount given by `self.mnt_id`.
      */
-    #[allow(dead_code)]
     fn open(&self, mount_fd: &impl AsRawFd, flags: libc::c_int) -> io::Result<File> {
         let ret = unsafe { open_by_handle_at(mount_fd.as_raw_fd(), &self.handle, flags) };
         if ret >= 0 {
@@ -99,5 +115,23 @@ impl FileHandle {
         } else {
             Err(io::Error::last_os_error())
         }
+    }
+
+    /**
+     * Open a file handle, using the given `mount_fds` hash map.
+     *
+     * Look up `self.mnt_id` in `mount_fds`, and pass the result to `self.open()`.
+     */
+    pub fn open_with_mount_fds(
+        &self,
+        mount_fds: &MountFds,
+        flags: libc::c_int,
+    ) -> io::Result<File> {
+        let mount_fds_locked = mount_fds.map.read().unwrap();
+        let mount_file = mount_fds_locked
+            .get(&self.mnt_id)
+            .ok_or_else(|| io::Error::from_raw_os_error(libc::ENODEV))?;
+
+        self.open(mount_file, flags)
     }
 }
