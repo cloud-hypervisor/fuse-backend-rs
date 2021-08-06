solcloud/virtiofsd

commit a80c3be3c59c9c2aaae3a90ebef2b72d151a2e4c

Create file handles
Now that we can work with file handles as alternatives to `O_PATH`
`File` objects, we can actually let the functions that create
`InodeData` objects try to generate such handles (before falling back to
`O_PATH` FDs).

The function we use for this (`FileHandle::from_name_at_with_mount_fds`)
checks `mount_fds` to see whether the handle's mount ID is already
mapped there, and if not, it will open the file for which it was
supposed to generate a handle, and put it into `mount_fds` to represent
this mount ID.  (Because this file must be in that mount.)

Generating handles is guarded by the `inode_file_handles` config option,
which is enabled with the `--inode-file-handles` command line switch.

Signed-off-by: Max Reitz <mreitz@redhat.com>

src/passthrough/file_handle.rs
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use crate::passthrough::stat::statx;
 use std::collections::HashMap;
 use std::ffi::CStr;
 use std::fs::File;
 use std::io;
-use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
 use std::sync::RwLock;
 
 const MAX_HANDLE_SZ: usize = 128;
@@ -61,7 +62,6 @@ impl MountFds {
 
 impl FileHandle {
     /// Create a file handle for the given file.
-    #[allow(dead_code)]
     fn from_name_at(dir: &impl AsRawFd, path: &CStr) -> io::Result<Self> {
         let mut mount_id: libc::c_int = 0;
         let mut c_fh = CFileHandle {
@@ -87,6 +87,59 @@ impl FileHandle {
         } else {
             Err(io::Error::last_os_error())
         }
+    }
+
+    /**
+     * Create a file handle for the given file, and ensure that `mount_fds` contains a valid fd for
+     * the mount the file is on (so that `handle.open_with_mount_fds()` will work).
+     *
+     * If `path` is empty, `reopen_dir` may be invoked to duplicate `dir` with custom
+     * `libc::open()` flags.
+     */
+    pub fn from_name_at_with_mount_fds<F>(
+        dir: &impl AsRawFd,
+        path: &CStr,
+        mount_fds: &MountFds,
+        reopen_dir: F,
+    ) -> io::Result<Self>
+    where
+        F: FnOnce(RawFd, libc::c_int) -> io::Result<File>,
+    {
+        let handle = Self::from_name_at(dir, path)?;
+
+        if !mount_fds.map.read().unwrap().contains_key(&handle.mnt_id) {
+            let file = if path.to_bytes().is_empty() {
+                // `open_by_handle_at()` needs a non-`O_PATH` fd, and `dir` may be `O_PATH`, so we
+                // have to open a new fd here
+                reopen_dir(
+                    dir.as_raw_fd(),
+                    libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+                )?
+            } else {
+                // `openat(dir, path)` should give us a file on the mount
+                let ret = unsafe {
+                    libc::openat(
+                        dir.as_raw_fd(),
+                        path.as_ptr(),
+                        libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+                    )
+                };
+                if ret < 0 {
+                    return Err(io::Error::last_os_error());
+                }
+                // `openat()` guarantees `ret` is a valid fd
+                unsafe { File::from_raw_fd(ret) }
+            };
+
+            // Ensure that `file` refers to an inode with the mount ID we need
+            if statx(&file, None)?.mnt_id != handle.mnt_id {
+                return Err(io::Error::from_raw_os_error(libc::EIO));
+            }
+
+            mount_fds.map.write().unwrap().insert(handle.mnt_id, file);
+        }
+
+        Ok(handle)
     }
 
     /**

src/passthrough/mod.rs
@@ -43,13 +43,11 @@ enum InodeAltKey {
         dev: libc::dev_t,
         mnt_id: u64,
     },
-    #[allow(dead_code)]
     Handle(FileHandle),
 }
 
 enum FileOrHandle {
     File(File),
-    #[allow(dead_code)]
     Handle(FileHandle),
 }
 
@@ -355,6 +353,18 @@ pub struct Config {
     ///
     /// The default is `true`.
     pub announce_submounts: bool,
+
+    /// Whether to use file handles to reference inodes.  We need to be able to open file
+    /// descriptors for arbitrary inodes, and by default that is done by storing an `O_PATH` FD in
+    /// `InodeData`.  Not least because there is a maximum number of FDs a process can have open
+    /// users may find it preferable to store a file handle instead, which we can use to open an FD
+    /// when necessary.
+    /// So this switch allows to choose between the alternatives: When set to `false`, `InodeData`
+    /// will store `O_PATH` FDs.  Otherwise, we will attempt to generate and store a file handle
+    /// instead.
+    ///
+    /// The default is `false`.
+    pub inode_file_handles: bool,
 }
 
 impl Default for Config {
@@ -370,6 +380,7 @@ impl Default for Config {
             xattr_security_capability: None,
             proc_sfd_rawfd: None,
             announce_submounts: true,
+            inode_file_handles: false,
         }
     }
 }
@@ -538,24 +549,40 @@ impl PassthroughFs {
 
         let p_file = p.get_file(&self.mount_fds)?;
 
-        // Safe because this doesn't modify any memory and we check the return value.
-        let fd = unsafe {
-            libc::openat(
-                p_file.as_raw_fd(),
-                name.as_ptr(),
-                libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
-            )
+        let handle = if self.cfg.inode_file_handles {
+            FileHandle::from_name_at_with_mount_fds(&p_file, name, &self.mount_fds, |fd, flags| {
+                reopen_fd_through_proc(&fd, flags, &self.proc_self_fd)
+            })
+        } else {
+            Err(io::Error::from_raw_os_error(libc::ENOTSUP))
         };
-        if fd < 0 {
-            return Err(io::Error::last_os_error());
-        }
 
-        // Safe because we just opened this fd.
-        let f = unsafe { File::from_raw_fd(fd) };
+        // Ignore errors, because having a handle is optional
+        let file_or_handle = if let Ok(h) = handle {
+            FileOrHandle::Handle(h)
+        } else {
+            // Safe because this doesn't modify any memory and we check the return value.
+            let fd = unsafe {
+                libc::openat(
+                    p_file.as_raw_fd(),
+                    name.as_ptr(),
+                    libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+                )
+            };
+            if fd < 0 {
+                return Err(io::Error::last_os_error());
+            }
+
+            // Safe because we just opened this fd.
+            FileOrHandle::File(unsafe { File::from_raw_fd(fd) })
+        };
 
         let mut attr_flags: u32 = 0;
 
-        let st = self.stat(&f, None)?;
+        let st = match &file_or_handle {
+            FileOrHandle::File(f) => self.stat(f, None)?,
+            FileOrHandle::Handle(_) => self.stat(&p_file, Some(name))?,
+        };
 
         if st.st.st_mode & libc::S_IFDIR != 0
             && self.announce_submounts.load(Ordering::Relaxed)
@@ -569,8 +596,10 @@ impl PassthroughFs {
             dev: st.st.st_dev,
             mnt_id: st.mnt_id,
         };
-        // TODO: Once we generate file handles, set this
-        let handle_altkey: Option<InodeAltKey> = None;
+        // Note that this will always be `None` if `cfg.inode_file_handles` is false, but we only
+        // really need this alt key when we do not have an `O_PATH` fd open for every inode.  So if
+        // `cfg.inode_file_handles` is false, we do not need this key anyway.
+        let handle_altkey = file_or_handle.handle().map(|h| InodeAltKey::Handle(*h));
 
         let data = {
             let inodes = self.inodes.read().unwrap();
@@ -609,7 +638,7 @@ impl PassthroughFs {
                 inode,
                 Arc::new(InodeData {
                     inode,
-                    file_or_handle: FileOrHandle::File(f),
+                    file_or_handle,
                     refcount: AtomicU64::new(1),
                     dev: st.st.st_dev,
                     mnt_id: st.mnt_id,
@@ -796,31 +825,54 @@ impl FileSystem for PassthroughFs {
     fn init(&self, capable: FsOptions) -> io::Result<FsOptions> {
         let root = CString::new(self.cfg.root_dir.as_str()).expect("CString::new failed");
 
-        // Safe because this doesn't modify any memory and we check the return value.
-        // We use `O_PATH` because we just want this for traversing the directory tree
-        // and not for actually reading the contents.
-        let fd = unsafe {
-            libc::openat(
-                libc::AT_FDCWD,
-                root.as_ptr(),
-                libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+        let handle = if self.cfg.inode_file_handles {
+            FileHandle::from_name_at_with_mount_fds(
+                &libc::AT_FDCWD,
+                &root,
+                &self.mount_fds,
+                |fd, flags| reopen_fd_through_proc(&fd, flags, &self.proc_self_fd),
             )
+        } else {
+            Err(io::Error::from_raw_os_error(libc::ENOTSUP))
         };
-        if fd < 0 {
-            return Err(io::Error::last_os_error());
-        }
 
-        // Safe because we just opened this fd above.
-        let f = unsafe { File::from_raw_fd(fd) };
+        // Ignore errors, because having a handle is optional
+        let file_or_handle = if let Ok(h) = handle {
+            FileOrHandle::Handle(h)
+        } else {
+            // Safe because this doesn't modify any memory and we check the return value.
+            // We use `O_PATH` because we just want this for traversing the directory tree
+            // and not for actually reading the contents.
+            let fd = unsafe {
+                libc::openat(
+                    libc::AT_FDCWD,
+                    root.as_ptr(),
+                    libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+                )
+            };
+            if fd < 0 {
+                return Err(io::Error::last_os_error());
+            }
 
-        let st = self.stat(&f, None)?;
+            // Safe because we just opened this fd above.
+            FileOrHandle::File(unsafe { File::from_raw_fd(fd) })
+        };
+
+        let st = match &file_or_handle {
+            FileOrHandle::File(f) => self.stat(f, None)?,
+            FileOrHandle::Handle(_) => self.stat(&libc::AT_FDCWD, Some(&root))?,
+        };
 
         // Safe because this doesn't modify any memory and there is no need to check the return
         // value because this system call always succeeds. We need to clear the umask here because
         // we want the client to be able to set all the bits in the mode.
         unsafe { libc::umask(0o000) };
 
-        let handle_altkey: Option<InodeAltKey> = None;
+        // Copy file handle (if there is one) before moving file_or_handle into the new InodeData
+        // object
+        // (This is always `None` if `cfg.inode_file_handles` is false.  As explained in
+        // `do_lookup()`, that is alright.)
+        let handle_altkey = file_or_handle.handle().map(|h| InodeAltKey::Handle(*h));
 
         let mut inodes = self.inodes.write().unwrap();
 
@@ -829,7 +881,7 @@ impl FileSystem for PassthroughFs {
             fuse::ROOT_ID,
             Arc::new(InodeData {
                 inode: fuse::ROOT_ID,
-                file_or_handle: FileOrHandle::File(f),
+                file_or_handle,
                 refcount: AtomicU64::new(2),
                 dev: st.st.st_dev,
                 mnt_id: st.mnt_id,

src/main.rs
@@ -343,6 +343,10 @@ struct Opt {
     /// Don't tell the guest which directories are mount points
     #[structopt(long)]
     no_announce_submounts: bool,
+
+    /// Use file handles to reference inodes instead of O_PATH file descriptors
+    #[structopt(long)]
+    inode_file_handles: bool,
 }
 
 fn main() {
@@ -374,6 +378,7 @@ fn main() {
             xattrmap,
             proc_sfd_rawfd: sandbox.get_proc_self_fd(),
             announce_submounts,
+            inode_file_handles: opt.inode_file_handles,
             ..Default::default()
         },
     };

cloud-hypervisor/fuse-backend-rs

commit c307a7abd8b368864e2597e5b81df2ce5664ad39

passthroughfs: Do not open special files for `mount_fds`
We must not open special files (i.e. anything but regular files and
directories) with any flag but `O_PATH`.  Right now,
`from_name_at_with_mount_fds()` opens files indiscriminately of their
type, and so this is wrong.

Instead, open the file with `O_PATH` first and use the information we
get from `statx()` (which we call anyway) to determine whether it is
safe to reopen this file with `O_RDONLY`.

Reported-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
Fixes: a80c3be3c59c9c2aaae3a90ebef2b72d151a2e4c
       ("Create file handles")
Signed-off-by: Max Reitz <mreitz@redhat.com>
Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>

src/passthrough/file_handle.rs
@@ -122,20 +122,23 @@ impl FileHandle {
         let handle = Self::from_name_at(dir, path)?;
 
         if !mount_fds.map.read().unwrap().contains_key(&handle.mnt_id) {
-            let file = if path.to_bytes().is_empty() {
+            let (path_fd, _path_file) = if path.to_bytes().is_empty() {
                 // `open_by_handle_at()` needs a non-`O_PATH` fd, and `dir` may be `O_PATH`, so we
                 // have to open a new fd here
-                reopen_dir(
-                    dir.as_raw_fd(),
-                    libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC,
-                )?
+                // We do not know whether `dir`/`path` is a special file, though, and we must not open
+                // special files with anything but `O_PATH`, so we have to get some `O_PATH` fd first
+                // that we can stat to find out whether it is safe to open.
+                // (When opening a new fd here, keep a `File` object around so the fd is closed when it
+                // goes out of scope.)
+                (dir.as_raw_fd(), None)
             } else {
-                PassthroughFs::open_file(
+                let f = PassthroughFs::open_file(
                     dir.as_raw_fd(),
                     path,
-                    libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+                    libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
                     0,
-                )?
+                )?;
+                (f.as_raw_fd(), Some(f))
             };
 
             // liubo: TODO find mnt id
@@ -143,6 +146,15 @@ impl FileHandle {
             // if statx(&file, None)?.mnt_id != handle.mnt_id {
             //     return Err(io::Error::from_raw_os_error(libc::EIO));
             // }
+
+            let st = PassthroughFs::stat(&path_fd, None)?;
+            // Ensure that we can safely reopen `path_fd` with `O_RDONLY`
+            let file_type = st.st_mode & libc::S_IFMT;
+            if file_type != libc::S_IFREG && file_type != libc::S_IFDIR {
+                return Err(io::Error::from_raw_os_error(libc::EIO));
+            }
+
+            let file = reopen_dir(path_fd, libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC)?;
 
             mount_fds.map.write().unwrap().insert(handle.mnt_id, file);
         }

src/passthrough/mod.rs
@@ -652,7 +652,16 @@ impl<D: AsyncDrive, S: BitmapSlice + Send + Sync> PassthroughFs<D, S> {
             FileHandle::from_name_at_with_mount_fds(&p_file, name, &self.mount_fds, |fd, flags| {
                 let pathname = CString::new(format!("self/fd/{}", fd.as_raw_fd()))
                     .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-                Self::open_file(self.proc.as_raw_fd(), &pathname, flags, 0)
+
+                // We don't really check `flags` because if the kernel can't handle poorly specified flags
+                // then we have much bigger problems. Also, clear the `O_NOFOLLOW` flag if it is set since
+                // we need to follow the `/proc/self/fd` symlink to get the file.
+                Self::open_file(
+                    self.proc.as_raw_fd(),
+                    &pathname,
+                    (flags | libc::O_CLOEXEC) & (!libc::O_NOFOLLOW),
+                    0,
+                )
                 // reopen_fd_through_proc(&fd, flags, &self.proc_self_fd)
             })
         } else {