cloud-hypervisor/fuse-backend-rs

commit 2895435c15e7e3a9db85ed5a27381be32b2405ee

passthrough: optimize async_do_getattr/do_getattr()
Only create a temporary file descriptor when an InodeData contains a
File Handle, otherwise directly use the File object contained in
InodeData::file_or_handle.

Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

src/passthrough/async_io.rs
@@ -186,25 +186,40 @@ impl<D: AsyncDrive, S: BitmapSlice + Send + Sync> PassthroughFs<D, S> {
         &self,
         ctx: &Context,
         inode: Inode,
+        handle: Option<<Self as FileSystem<S>>::Handle>,
     ) -> io::Result<(libc::stat64, Duration)> {
+        let st;
+        let fd;
         let data = self.inode_map.get(inode).map_err(|e| {
             error!("fuse: do_getattr ino {} Not find err {:?}", inode, e);
             e
         })?;
-        let file = data.async_get_file(&self.mount_fds).await?;
 
-        let st = self
-            .async_stat(ctx, file.get_file_ref(), None)
-            .await
-            .map_err(|e| {
-                error!(
-                    "fuse: do_getattr stat failed ino {} fd: {:?} err {:?}",
-                    inode,
-                    file.as_raw_fd(),
-                    e
-                );
-                e
-            })?;
+        if let Some(h) = handle {
+            let hd = self.handle_map.get(h, inode)?;
+            fd = hd.get_handle_raw_fd();
+            st = self.async_stat_fd(ctx, fd, None).await;
+        } else {
+            match &data.file_or_handle {
+                FileOrHandle::File(f) => {
+                    fd = f.as_raw_fd();
+                    st = self.async_stat_fd(ctx, fd, None).await;
+                }
+                FileOrHandle::Handle(_h) => {
+                    let file = data.async_get_file(&self.mount_fds).await?;
+                    fd = file.as_raw_fd();
+                    st = self.async_stat_fd(ctx, fd, None).await;
+                }
+            }
+        }
+
+        let st = st.map_err(|e| {
+            error!(
+                "fuse: do_getattr stat failed ino {} fd: {:?} err {:?}",
+                inode, fd, e
+            );
+            e
+        })?;
 
         Ok((st, self.cfg.attr_timeout))
     }
@@ -368,10 +383,9 @@ impl<D: AsyncDrive + Sync, S: BitmapSlice + Send + Sync> AsyncFileSystem<D, S>
         &self,
         ctx: Context,
         inode: <Self as FileSystem<S>>::Inode,
-        _handle: Option<<Self as FileSystem<S>>::Handle>,
+        handle: Option<<Self as FileSystem<S>>::Handle>,
     ) -> io::Result<(libc::stat64, Duration)> {
-        // TODO: optimize async_do_getattr()
-        self.async_do_getattr(&ctx, inode).await
+        self.async_do_getattr(&ctx, inode, handle).await
     }
 
     async fn async_setattr(
@@ -517,7 +531,7 @@ impl<D: AsyncDrive + Sync, S: BitmapSlice + Send + Sync> AsyncFileSystem<D, S>
             }
         }
 
-        self.async_do_getattr(&ctx, inode).await
+        self.async_do_getattr(&ctx, inode, handle).await
     }
 
     async fn async_open(

src/passthrough/sync_io.rs
@@ -207,19 +207,40 @@ impl<D: AsyncDrive, S: BitmapSlice + Send + Sync> PassthroughFs<D, S> {
         Ok((Some(handle), opts))
     }
 
-    fn do_getattr(&self, inode: Inode) -> io::Result<(libc::stat64, Duration)> {
+    fn do_getattr(
+        &self,
+        inode: Inode,
+        handle: Option<Handle>,
+    ) -> io::Result<(libc::stat64, Duration)> {
+        let st;
+        let fd;
         let data = self.inode_map.get(inode).map_err(|e| {
             error!("fuse: do_getattr ino {} Not find err {:?}", inode, e);
             e
         })?;
 
-        let file = data.get_file(&self.mount_fds)?;
-        let st = Self::stat(&file, None).map_err(|e| {
+        if let Some(h) = handle {
+            let hd = self.handle_map.get(h, inode)?;
+            fd = hd.get_handle_raw_fd();
+            st = Self::stat_fd(fd, None);
+        } else {
+            match &data.file_or_handle {
+                FileOrHandle::File(f) => {
+                    fd = f.as_raw_fd();
+                    st = Self::stat_fd(fd, None);
+                }
+                FileOrHandle::Handle(_h) => {
+                    let file = data.get_file(&self.mount_fds)?;
+                    fd = file.as_raw_fd();
+                    st = Self::stat_fd(fd, None);
+                }
+            }
+        }
+
+        let st = st.map_err(|e| {
             error!(
                 "fuse: do_getattr stat failed ino {} fd: {:?} err {:?}",
-                inode,
-                file.as_raw_fd(),
-                e
+                inode, fd, e
             );
             e
         })?;
@@ -650,9 +671,9 @@ impl<D: AsyncDrive, S: BitmapSlice + Send + Sync> FileSystem<S> for PassthroughF
         &self,
         _ctx: Context,
         inode: Inode,
-        _handle: Option<Handle>,
+        handle: Option<Handle>,
     ) -> io::Result<(libc::stat64, Duration)> {
-        self.do_getattr(inode)
+        self.do_getattr(inode, handle)
     }
 
     fn setattr(
@@ -797,7 +818,7 @@ impl<D: AsyncDrive, S: BitmapSlice + Send + Sync> FileSystem<S> for PassthroughF
             }
         }
 
-        self.do_getattr(inode)
+        self.do_getattr(inode, handle)
     }
 
     fn rename(

src/passthrough/mod.rs
@@ -101,16 +101,6 @@ impl AsRawFd for InodeFile<'_> {
     }
 }
 
-#[cfg(feature = "async-io")]
-impl InodeFile<'_> {
-    fn get_file_ref(&self) -> &File {
-        match self {
-            Self::Owned(f) => f,
-            Self::Ref(f) => *f,
-        }
-    }
-}
-
 struct InodeData {
     inode: Inode,
     // Most of these aren't actually files but ¯\_(ツ)_/¯.

commit 2de5e722395d0342462e2459ee00fe09cdc0a4d5

passthrough: hornor no_open in do_getattr()
Otherwise we'll get EBADF failure as we can't find handle in handle map.

Fixes: 2895435c15e7 ("passthrough: optimize async_do_getattr/do_getattr()")
Signed-off-by: Eryu Guan <eguan@linux.alibaba.com>

src/passthrough/async_io.rs
@@ -221,8 +221,11 @@ impl<D: AsyncDrive> PassthroughFs<D> {
             e
         })?;
 
-        if let Some(h) = handle {
-            let hd = self.handle_map.get(h, inode)?;
+        // kernel sends 0 as handle in case of no_open, and it depends on fuse server to handle
+        // this case correctly.
+        if !self.no_open.load(Ordering::Relaxed) && handle.is_some() {
+            // Safe as we just checked handle
+            let hd = self.handle_map.get(handle.unwrap(), inode)?;
             fd = hd.get_handle_raw_fd();
             st = self.async_stat_fd(ctx, fd, None).await;
         } else {

src/passthrough/sync_io.rs
@@ -222,8 +222,11 @@ impl<D: AsyncDrive> PassthroughFs<D> {
             e
         })?;
 
-        if let Some(h) = handle {
-            let hd = self.handle_map.get(h, inode)?;
+        // kernel sends 0 as handle in case of no_open, and it depends on fuse server to handle
+        // this case correctly.
+        if !self.no_open.load(Ordering::Relaxed) && handle.is_some() {
+            // Safe as we just checked handle
+            let hd = self.handle_map.get(handle.unwrap(), inode)?;
             fd = hd.get_handle_raw_fd();
             st = Self::stat_fd(fd, None);
         } else {