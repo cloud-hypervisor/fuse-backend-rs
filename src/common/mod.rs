4878fb5238a03994ceea0b059ff955f97ca3abfb
wllenyj <wllenyj@linux.alibaba.com>
2022-07-12 23:38:55 +0800

    tokio-uring: temporarily include tokio-uring into common module
    Tempoarily include tokio-uring into dbs-fuse, so we could publish
    dbs-fuse. We will switch to upstream crate once our enhancements
    have been merged and new version is available.
    
    Signed-off-by: wllenyj <wllenyj@linux.alibaba.com>
    Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -6,3 +6,10 @@
 
 pub mod file_buf;
 pub mod file_traits;
+
+// Temporarily include all source code tokio-uring.
+// Will switch to upstream once our enhancement have been merged and new version available.
+#[cfg(all(feature = "async-io", target_os = "linux"))]
+pub mod tokio_uring;
+#[cfg(all(feature = "async-io", target_os = "linux"))]
+pub(crate) use self::tokio_uring::{buf, driver, fs, future, BufResult};
