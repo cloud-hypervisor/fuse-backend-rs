cloud-hypervisor/fuse-backend-rs

commit 345bc0924299ebe9a92210403c10648ba3d5f8e9

Vfs: remove no open flags correctly
When vfs options has no_open as false, we need to remove
ZERO_MESSAGE_OPEN from "capability" passed down to passthroughfs,
otherwise, passthroughfs would always enables no_open.

Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>

src/api/vfs/sync_io.rs
@@ -22,6 +22,8 @@ impl<D: AsyncDrive, S: BitmapSlice> FileSystem<S> for Vfs<D, S> {
             n_opts.no_open = !(opts & FsOptions::ZERO_MESSAGE_OPEN).is_empty();
             // We can't support FUSE_ATOMIC_O_TRUNC with no_open
             n_opts.out_opts.remove(FsOptions::ATOMIC_O_TRUNC);
+        } else {
+            n_opts.out_opts.remove(FsOptions::ZERO_MESSAGE_OPEN);
         }
         n_opts.no_opendir = !(opts & FsOptions::ZERO_MESSAGE_OPENDIR).is_empty();
         if n_opts.no_writeback {

src/passthrough/mod.rs
@@ -851,9 +851,46 @@ fn ebadf() -> io::Error {
 mod tests {
     use super::*;
     use crate::api::filesystem::*;
+    use crate::api::{Vfs, VfsOptions};
     use flexi_logger::{self, colored_opt_format, Logger};
     use log;
+    use std::ops::Deref;
     use vmm_sys_util::{tempdir::TempDir, tempfile::TempFile};
+
+    fn passthroughfs_no_open(cfg: bool) {
+        let opts = VfsOptions {
+            no_open: cfg,
+            ..Default::default()
+        };
+
+        let vfs = &Vfs::new(opts);
+        // Assume that fuse kernel supports no_open.
+        vfs.init(FsOptions::ZERO_MESSAGE_OPEN).unwrap();
+
+        let fs_cfg = Config {
+            do_import: false,
+            no_open: cfg,
+            ..Default::default()
+        };
+        let fs = PassthroughFs::new(fs_cfg.clone()).unwrap();
+        fs.import().unwrap();
+        vfs.mount(Box::new(fs), "/submnt/A").unwrap();
+
+        let p_fs = vfs.get_rootfs("/submnt/A").unwrap().unwrap();
+        let any_fs = p_fs.deref().as_any();
+        any_fs
+            .downcast_ref::<PassthroughFs>()
+            .map(|fs| {
+                assert_eq!(fs.no_open.load(Ordering::Relaxed), cfg);
+            })
+            .unwrap();
+    }
+
+    #[test]
+    fn test_passthroughfs_no_open() {
+        passthroughfs_no_open(true);
+        passthroughfs_no_open(false);
+    }
 
     #[test]
     fn test_passthroughfs_inode_file_handles() {

commit cbbde3cb2692ab77ce9572db0736e3bf6f42744a

Vfs: remove no opendir flags correctly
Like the commit 345bc0924299ebe9a92210403c10648ba3d5f8e9, we should also
remove ZERO_MESSAGE_OPENDIR when no_opendir is disabled, otherwise,
passthroughfs would always set its no_opendir to true.

Fixes: 345bc0924299ebe9a92210403c10648ba3d5f8e9
Reported-by: Daowen Luo <luodaowen.backend@bytedance.com>
Signed-off-by: Jiachen Zhang <zhangjiachen.jaycee@bytedance.com>

src/api/vfs/sync_io.rs
@@ -25,7 +25,11 @@ impl FileSystem for Vfs {
         } else {
             n_opts.out_opts.remove(FsOptions::ZERO_MESSAGE_OPEN);
         }
-        n_opts.no_opendir = !(opts & FsOptions::ZERO_MESSAGE_OPENDIR).is_empty();
+        if n_opts.no_opendir {
+            n_opts.no_opendir = !(opts & FsOptions::ZERO_MESSAGE_OPENDIR).is_empty();
+        } else {
+            n_opts.out_opts.remove(FsOptions::ZERO_MESSAGE_OPENDIR);
+        }
         if n_opts.no_writeback {
             n_opts.out_opts.remove(FsOptions::WRITEBACK_CACHE);
         }