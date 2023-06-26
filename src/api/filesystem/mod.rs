d80d61ea78ecd06da8d211c1964fa32f2dd5b652
Carlos López <carlos.lopez@suse.com>
2023-06-26 12:46:18 +0200

    abi: implement Debug for Entry struct
    Signed-off-by: Carlos López <carlos.lopez@suse.com>

--- src/api/filesystem/mod.rs
+++ src/api/filesystem/mod.rs
@@ -31,7 +31,7 @@ mod sync_io;
 pub use sync_io::FileSystem;
 
 /// Information about a path in the filesystem.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct Entry {
     /// An `Inode` that uniquely identifies this path. During `lookup`, setting this to `0` means a
     /// negative entry. Returning `ENOENT` also means a negative entry but setting this to `0`
