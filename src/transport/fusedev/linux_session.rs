db54205f5b7277184884b514f654806b0a1c9c1f
xujihui1985 <xujihui1985@gmail.com>
2024-03-17 20:02:19 +0800

    refactor: extract with_writer on FuseSession for notify api

--- src/transport/fusedev/linux_session.rs
+++ src/transport/fusedev/linux_session.rs
@@ -238,6 +238,19 @@ impl FuseSession {
         }
     }
 
+    /// Create a new fuse message channel with a specific buffer size.
+    pub fn with_writer<F>(&mut self, f: F)
+    where
+        F: FnOnce(FuseDevWriter),
+    {
+        if let Some(file) = &self.file {
+            let fd = file.as_raw_fd();
+            let mut buf = vec![0x0u8; self.bufsize];
+            let writer = FuseDevWriter::new(fd, &mut buf).unwrap();
+            f(writer);
+        }
+    }
+
     /// Wake channel loop and exit
     pub fn wake(&self) -> Result<()> {
         let wakers = self
